81bd1ac4a9b216dbe74f90a63309809f2ab42538
ThunderRedStar <wvongphanith30@stuy.edu>
2022-03-21 09:27:26 -0400

    dependencies

---
+++ src/index.js
@@ -0,0 +1,24 @@
+/**
+ * ThunderCMS
+ */
+
+// External dependencies
+
+// These are all tentative, and will be changed as we go along. To activate a dependency, put a slash directly after the first asterisk in the line.
+/*
+ */ const express = require('express');                        /*
+ * const bodyParser = require('body-parser');                  /*
+ * const cookieParser = require('cookie-parser');              /*
+ * const session = require('express-session');                 /*
+ * const passport = require('passport');                       /*
+ * const LocalStrategy = require('passport-local').Strategy;   /*
+ * const cluster = require('cluster');                         /*
+ * const fs = require('fs');                                   /*
+ * const path = require('path');                               /*
+ * const crypto = require('crypto');                           /*
+ * const bcrypt = require('bcrypt');                           /*
+ * const sqlite3 = require('sqlite3');                         /*
+ * const { exec } = require('child_process');                  /*
+ * // Currently infected with malware, will re-add once I can pin it to 9.1.2
+ * // const IPC = require('node-ipc');                         /*
+ */
